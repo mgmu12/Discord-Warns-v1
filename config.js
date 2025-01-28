3daa9a5e719591c0c8984a81b24a1ad30fb0c56d
mgmu12 <mgmg12mmu@gmail.com>
2025-01-28 18:52:03 +0300

    Update config.js
    sd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-clientId: "", // ايدي البوت
-    guildId: "", // ايدي السيرفر
-    token: "" // توكن البوت
+clientId: "1326162716033028167", // ايدي البوت
+    guildId: "1333820131817820180", // ايدي السيرفر
+    token: "MTMyNjE2MjcxNjAzMzAyODE2Nw.G0kYbj.yIAYUx32ybTYEaSnb77Sxau3dL2GTjY4j1fvwE" // توكن البوت
 }
